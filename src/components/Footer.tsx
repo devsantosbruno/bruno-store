b0e1fc36576eb1ca155c72f534e72ba1638c857a
bruno santos <devbrunosantos@gmail.com>
2023-11-07 08:12:23 -0300

    feat: Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
+      <p>
+        © 2023 Copyright <span className="font-semibold">FSW Store</span>
+      </p>
+    </div>
+  );
+}
