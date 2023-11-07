2855fe11208896819c50bf0bfe03a692d774c5f8
bruno santos <devbrunosantos@gmail.com>
2023-11-07 08:20:18 -0300

    style: ensure footer is always on the bottom

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
       <p>
-        © 2023 Copyright <span className="font-semibold">FSW Store</span>
+        © 2023 Copyright <span className="font-semibold">Bruno Store</span>
       </p>
     </div>
   );
